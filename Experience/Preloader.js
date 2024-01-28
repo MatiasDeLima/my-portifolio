b64b0504b922556e480da9fad6bbe083871e0688
Lucas Matias <101410443+MatiasDeLima@users.noreply.github.com>
2024-01-27 22:20:35 -0300

    feat() create preloader

---
+++ Experience/Preloader.js
@@ -0,0 +1,29 @@
+import { EventEmitter } from "events";
+import Experience from "./Experience.js";
+
+export default class Preloader extends EventEmitter {
+    constructor() {
+        super();
+        this.experience = new Experience();
+        this.scene = this.experience.scene;
+        this.sizes = this.experience.sizes;
+        this.resources = this.experience.resources;
+        this.camera = this.experience.camera;
+        this.world = this.experience.world;
+
+        this.world.on("worldready", () => {
+            this.setAssets();
+            this.playIntro();
+        })
+    }
+
+    setAssets() {
+        this.room = this.experience.world.room.actualRoom;
+        this.roomChildren = this.experience.world.room.roomChildren;
+        console.log(this.roomChildren);
+    }
+
+    playIntro() {
+
+    }
+}
